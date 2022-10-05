5b52746d0e3b054bc21e90853596236a2aeaa6c4
Saidamirxon <99072823+NurmatovAmeer@users.noreply.github.com>
2022-10-05 21:42:39 +0500

    redux add

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
 import Layout from "../layouts/layout";
 import "../scss/globals.scss";
+import { Provider } from 'react-redux';
+import {initStore} from '../redux/rootReducer'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="app">
+    <Provider store={initStore}>
+      <div className="app">
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </div>
+    </Provider>
   );
 }
 
